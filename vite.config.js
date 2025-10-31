c1cbbe2a57c035680db3618fc5c0eacfbcc80210
Naim <naim123nono@gmail.com>
2025-10-31 19:04:34 +0100

    Configure vite.config.js with Vue plugin and ensure correct app entry

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,14 @@ export default defineConfig({
       input: ['resources/css/app.css', 'resources/js/app.js'],
       refresh: true,
     }),
-    vue(),
+    vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
   ],
   resolve: {
     alias: {
